effab5325d44ab4da592e832f378a82e7738dd4c
zzzpz <1911293650@qq.com>
2025-09-09 17:03:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if weight_str[-2:] == "kg":
     kg = float(weight_str[:-2])
     pd = kg * kg_to_pd
     print(f"对应的英制重量为{pd:.3f}磅")
-elif weight_str[-2:] == "pd":
+elif weight_str[-2:] == "kg":
     pd = float(weight_str[:-2])
     kg = pd / kg_to_pd
     print(f"对应的公制重量为{kg:.3f}公斤")
