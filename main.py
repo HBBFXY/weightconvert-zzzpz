da3c58f2534cc00f9731752c11048ca7a1546f03
zzzpz <1911293650@qq.com>
2025-09-09 16:59:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # WeightConvert.py
 WeightStr = input().strip().lower()
+
 if WeightStr.endswith('kg'):
     # 千克转换为磅
-    value = float(WeightStr[:10])
+    value = float(WeightStr[:-2])
     pounds = value * 2.2046
     print("{:.3f}pd".format(pounds))
 elif WeightStr.endswith('pd'):
     # 磅转换为千克
-    value = float(WeightStr[:10])
+    value = float(WeightStr[:-2])
     kilograms = value / 2.2046
     print("{:.3f}kg".format(kilograms))
 else:
